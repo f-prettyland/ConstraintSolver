834cfc7d68025ed2402249ff86b19df875333a33
liberte <fp26@st-andrews.ac.uk>
2016-04-06 08:34:41 +0100

    Dynamic domains, further constraint type work

--- Solver.hs
+++ Solver.hs
@@ -5,7 +5,7 @@ import Data.Text
 --a variable and what it is bonded by
 data Constraint = Constraint Int [Int] [(Com, Int)]
 
-data LongConstraint = LongConstraint String (Maybe [Int]) (Maybe [(Com, String)])
+data LongConstraint = LongConstraint String (Maybe [Int]) (Maybe (Com, String))
 
 data Com = Com (a -> a -> Bool)
 
@@ -14,17 +14,18 @@ solve :: [(Int,[Int],[Int])] -> [Int]
 branch :: [Int] -> [(Int,[In)] -> [Int]
 
 
-parseLines :: [String] -> [(String, [Int])]
+parseLines :: [String] -> [LongConstraint]
+parseLines [] = []
 parseLines (s:ss)
-	| True == isVar  = ( varDomLine s : otherLines )
-	| False == isVar = (  : otherLines )
+	| True == isDom  = ( domLine s : otherLines )
+	| False == isDom = ( constLine  s : otherLines )
 	where
-	isVar = (length s) == 4
+	isDom = (length s) == 4
 	otherLines = parseLines ss
 
 --gets the domain and variable name for a line
-varDomLine :: String -> LongConstraint (String, [Int])
-varDomLine line = split!!0 dom Nothing
+domLine :: String -> LongConstraint (String, [Int])
+domLine line = split!!0 dom Nothing
 	where
 	split = spliceOn " " line
 	dom = domGet (split!!2)
@@ -32,17 +33,26 @@ varDomLine line = split!!0 dom Nothing
 --todo: parse for .. vs , for making range vs individual values
 --gets the domain that a variable can span from a string
 domGet :: String -> [Int]
-domGet dom = [low..high]
+domGet dom
+	| dom =~ ","  = nonConsecDomGet (spliceOn "," dom)
+	| otherwise   = [low..high]
 	where
 	split = spliceOn ".." dom
 :	low = read (split!!0)
 	high = read (split!!1)
 
+--builds the domain of possible numbers
+nonConsecDomGet :: [String] -> [Int]
+nonConsecDomGet [] = []
+nonConsecDomGet (s:ss) = ((read s)::Int : nonConsecDomGet ss )
+
+
 --gets the constraint from a line
 constLine :: String -> LongConstraint
-constLine line = 
+constLine line = split!!0 Nothing (op, split!!1)
 	where
 	split = spliceOn " " line
+	op = getOp split!!1
 
 --takes in a string and finds the operator for it
 getOp :: String -> Com

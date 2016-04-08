cd7abf2d4f91d963efde12009ea8580131a64d87
f-prettyland <fp26@st-andrews.ac.uk>
2016-04-08 17:58:29 +0100

    Solver almost propegating

--- Solver.hs
+++ Solver.hs
@@ -1,4 +1,5 @@
 module Solver where
+import Data.Maybe (fromJust)
 
 newtype VarId	 = VarId Int
 newtype Domain    = Domain [Int]
@@ -6,7 +7,13 @@ type Oper	 = (Int->Int->Int)
 type Equ     = (Int->Int->Bool)
 
 data Variable 		= Variable String Domain
-data VariableValues = VariableValue String Int
+data VariableValue = VariableValue String Int
+nameOf :: VariableValue -> String
+nameOf (VariableValue nam _) = nam
+
+valOf :: VariableValue -> Int
+valOf (VariableValue _ val) = val
+
 data Expr = VI String
 	| Term Int
 	| Form Expr Oper Expr
@@ -18,27 +25,75 @@ solveIt :: ([Constraint],[Variable],[Orr]) -> Maybe [VariableValue]
 
 --	    current assignment  cnsts to sat    vars n doms   ORcnst   Possible succesful var allocation
 solveIt' :: [VariableValues] -> [Constraint] -> [Variable] -> [Orr] -> Maybe [VariableValue]
-solveIt' cs vs or un
-	| sfd == Just True	= 
+solveIt' vv cs vs or un
+	| sfd == Just True	= vv
 	| sfd == Just False	= Nothing
 	| sfd == Nothing	= 
 	where
 	--checks if is is satified or satifiable
 	satfied = 
 
-
-
-evCon :: [VariableValues] -> Constraint -> Bool
-evCon vv (Constraint ex1 eqOp ex2) = eqOp  (evEx vv ex1) (evEx vv ex2)
-
-evEx :: [VariableValues] -> Expr -> Int
-evEx vv (Term t) 			= t
-evEx vv (Form ex1 op ex2)	= evald
+arcsConsistent :: [(String, String)] -> [Constraint] -> [Variable] -> [Variable]
+arcsConsistent [] cs vs = vs
+arcsConsistent ((src,dest):arcs) cs vs
+	| numOfCons > 0 = 
+	| otherwise		= arcsConsistent arcs cs vs
 	where
-	evald = evFrm vv ex1 op ex2
+	consToCheck = getConstraintsFor [] (src,dest) cs  
+	numOfCons	= length consToCheck 
 
-evFrm :: [VariableValues] -> Expr -> Oper -> Expr -> Int
-evFrm vv ex1 op ex2 = op rl1 rl2
+arcConsistent :: (String, String) -> [Constraint] -> [Variable] -> [Variable]
+arcConsistent (src,dst) [] vs = vs
+arcConsistent (src,dst) (c:cs) vs
+	| evCon 
 	where
-	rl1 = evEx vv ex1
-	rl2 = evEx vv ex2
+	/\[vv]/\ = ()
+
+--return constraints with these input variables in them
+--					soFarFound		vars2Find			allConst		Constrains with vars 
+getConstraintsFor :: [Constraint] -> (String, String) -> [Constraints] -> [Constraint]
+getConstraintsFor conCons (src, dst) (c:cs)
+	| hasBoth	= getConstraintsFor (c:conCons) (src,dst) cs
+	| otherwise	= getConstraintsFor conCons (src,dst) cs
+	where
+	Constraint ex1 op ex2 = c
+	hasBoth = ((exprContainsVar src ex1) && (exprContainsVar dst ex2)) || ((exprContainsVar dst ex1) && (exprContainsVar src ex2)) || ((exprContainsVar src ex1) && (exprContainsVar dst ex1)) || ((exprContainsVar src ex2) && (exprContainsVar dst ex2))
+
+
+exprContainsVar :: String -> Expr -> Bool
+exprContainsVar v (Term t)		= False
+exprContainsVar v (VI name)
+	| v == name = True
+	| otherwise = False
+exprContainsVar v (Form ex1 op ex2)	= (exprContainsVar ex1) || (exprContainsVar ex2)
+
+--todo, deal with unary
+--Finds out if the constraints are still satifiable with 
+--nodeConsistent :: [VariableValues] -> [Constraint] -> [Orr] -> Maybe Bool
+
+getVarVal :: [VariableValue] -> String -> Maybe Int
+getVarVal [] s = Nothing
+getVarVal (v:vs) s
+	| s == nameOf v	= Just (valOf v)
+	| otherwise	= getVarVal vs s
+
+evCon :: [VariableValue] -> Constraint -> Bool
+evCon vv (Constraint ex1 eqOp ex2)
+	| ((val1 == Nothing) || (val2 == Nothing)) = True
+	| otherwise				   = eqOp  (fromJust val1) (fromJust val2)
+	where
+	val1 = (evEx vv ex1) 
+	val2 = (evEx vv ex2)
+
+evEx :: [VariableValue] -> Expr -> Maybe Int
+evEx vv (Term t) 		= (Just t)
+evEx vv (VI name)		= (getVarVal vv name) 
+evEx vv (Form ex1 op ex2)	= (evFrm vv ex1 op ex2)
+
+evFrm :: [VariableValue] -> Expr -> Oper -> Expr -> Maybe Int
+evFrm vv ex1 op ex2
+	| ((val1 == Nothing) || (val2 == Nothing)) = Nothing
+	| otherwise				   = Just (op (fromJust val1) (fromJust val2))
+	where
+	val1 = evEx vv ex1
+	val2 = evEx vv ex2
